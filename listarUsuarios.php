fc0123743d99860360d734c7c60ebefe430ae4a5
omartins-zs <gabrielmatheus209@gmail.com>
2022-03-02 10:49:54 -0300

    Criando Paginas de Listagem dos Usuarios

---
+++ listarUsuarios.php
@@ -0,0 +1,28 @@
+<?php
+
+include_once('conexao.php');
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="./css/style.css">
+
+    <title>Lista de usuarios</title>
+</head>
+
+<body>
+    <center>
+
+        <h1>Usuarios cadastrados no Sistema</h1>
+
+    </center>
+
+</body>
+
+</html>
