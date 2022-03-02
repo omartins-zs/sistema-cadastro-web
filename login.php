99c6592a2b6d0b498dc6e2183bbfce16454e145a
omartins-zs <gabrielmatheus209@gmail.com>
2022-03-02 10:23:28 -0300

    Criando Restrição de Acesso Login

---
+++ login.php
@@ -0,0 +1,12 @@
+<?php
+
+include_once('conexao.php');
+
+$usuario = $_POST['txt_usuario'];
+$senha = $_POST['txt_senha'];
+
+$sql_logar = mysqli_query($conn, " SELECT * FROM usuarios WHERE usuario='$usuario' and '$senha'");
+
+if (mysqli_num_rows($sql_logar) != 0) {
+    header('Location : home.php');
+}
