63f2a8564aaed787e5db8907d4e7a811e677b67a
omartins-zs <gabrielmatheus209@gmail.com>
2022-03-02 09:40:50 -0300

    Criando Arquivo de Conexao com o BD

---
+++ conexao.php
@@ -0,0 +1,12 @@
+<?php
+
+// 2. Conexao com banco de dados 
+$conn = mysqli_connect('localhost', 'root', '', 'escola');
+
+// $servername = "localhost";
+// $username = "root";
+// $password = "";
+// <!-- 
+// new PDO("mysql:host=$servername;dbname=restaurante-bd", $username, $password);
+// $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+// //   echo "Conexão realizada com sucesso"; -->
