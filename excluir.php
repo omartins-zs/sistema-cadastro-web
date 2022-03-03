e81f8aaf3d102d11d692a501c5bbbd9ec57be1a4
omartins-zs <gabrielmatheus209@gmail.com>
2022-03-03 09:08:26 -0300

    Adicionando Mensagem de Return ao Excluir Usuario

--- excluir.php
+++ excluir.php
@@ -5,3 +5,15 @@ include_once('conexao.php');
 $id = $_GET['codigo'];
 
 $sql_eliminar = mysqli_query($conn, "DELETE FROM usuarios WHERE id='$id' ");
+
+if ($sql_eliminar == true) {
+    echo "<script>
+        alert('Usuario foi excluido com Sucesso!!');
+        window.location.href='listarUsuarios.php';
+    </script>";
+} else {
+    echo "<script>
+        alert('Falha ao excluir usuario!');
+        window.location.href='listarUsuarios.php';
+    </script>";
+}
