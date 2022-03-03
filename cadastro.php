35435aec64a4789dcc2de7679926fce95064b97e
omartins-zs <gabrielmatheus209@gmail.com>
2022-03-03 08:28:35 -0300

    Ajustes para cadastrar Nivel

--- cadastro.php
+++ cadastro.php
@@ -5,7 +5,7 @@ require_once('conexao.php');
 $usuario = $_POST['txt_usuario'];
 $senha = $_POST['txt_senha'];
 $email = $_POST['txt_email'];
-$nivel = $_POST['txt_nivel'];
+$nivel = $_POST['nivel'];
 
 $sql_cadastro = mysqli_query($conn, "INSERT INTO usuarios ( usuario, senha, email, nivel) VALUES ( '$usuario', '$senha', '$email', '$nivel')");
 
