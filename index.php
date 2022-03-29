e2b4e45218973faf3f1ae012729829e6e42acbd0
omartins-zs <gabrielmatheus209@gmail.com>
2022-03-29 09:09:05 -0300

    Personalizando tela de login com formulario atualizado

--- index.php
+++ index.php
@@ -1,37 +1,75 @@
+
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
+    <!-- CSS -->
     <link rel="stylesheet" href="./css/style.css">
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap" rel="stylesheet">
+
+    <!-- Icons -->
+    <script src="https://kit.fontawesome.com/cba92a6c1e.js" crossorigin="anonymous" defer></script>
 
     <title>Acesso</title>
 </head>
 
 <body>
 
-    <h1>TECNOLOGIA DA INFORMAÇÃO</h1>
-    <hr>
-    <h2>Tela de Login / Acesso</h2>
+    <header>
+        <h2>Tela de Login / Acesso</h2>
+    </header>
 
+    <main>
+    <form action="login.php" method="post">
+            <section class="inputs-container">
+                <label for="txt_usuario">Usuario</label>
+                <input type="text" placeholder="jorginho" name="txt_usuario"> <br>
 
-    <div class="login">
-        <form action="login.php" method="post">
-            <h3>Faça seu login</h3>
+                <div class="password-container">
 
-            Usuário: <br>
-            <input type="text" name="txt_usuario"> <br>
-            Senha: <br>
-            <input type="password" name="txt_senha" id=""> <br>
-            <br>
-            <input type="submit" value="Entrar"> <br>
-            <br>
-            <a href="./cadastroUsuario.php">Cadastrar-se</a>
-        </form>
-    </div>
+                    <input type="password" id="field-password" name="txt_senha" class="field-password" placeholder="*******">
+                    <i class="fas fa-eye" id="eye" onclick="showPassword()"></i>
+                    <i class="fas fa-eye-slash" id="eye-slash" onclick="showPassword()"></i>
+                </div>
+            </section>
+
+            <section class="password-infos">
+                <div>
+                    <input type="checkbox">
+                    <span>Lembrar senha?</span>
+                </div>
+
+                <a href="#">Esqueceu sua senha?</a>
+            </section>
+
+            <button type="submit" value="Entrar" id="btn-login">Login</button>
+
+    <section class="links-container">
+                <span>Ou entre com:</span>
+
+                <aside>
+                    <a href="#"><i class="fab fa-google-plus-square google"></i></a>
+                    <a href="#"><i class="fab fa-linkedin linkedin"></i></a>
+                    <a href="#"><i class="fab fa-facebook-square facebook"></i></a>
+                </aside>
+            </section>
+    <footer>
+                <hr>
+                <br>
+                <br>
+                <span>Ainda não tem uma conta? <a href="./cadastroUsuario.php">Cadastrar-se</a></span>
+            </footer>
+    </form>
+    </main>
+        <!-- Js -->
+    <script src="./js/index.js"></script>
 </body>
 
 </html>
